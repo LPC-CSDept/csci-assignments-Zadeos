ff4f587165258059cf8e1113774360fd529d6cd6
Zadeos <masonjlewis@outlook.com>
2021-12-16 16:04:27 -0800

    added polling

--- Final/final1.asm
+++ Final/final1.asm
@@ -12,10 +12,18 @@ pr1:	.asciiz "please enter 3 numbers (e.g. 123): "
 	.text
 	.globl main
 main:
-	li	$v0, 4		# print_string sys call code
-	la	$a0, pr1	# address of prompt
+	li	$v0, 4			# print_string sys call code
+	la	$a0, pr1		# address of prompt
 	syscall
 
-	li	$v0, 10		# exit sys call code
+	lui	$t0,  0xFFFF		# Receiver Control memory location
+rd_wait:
+	lw	$t1, 0($t0)		# load from receiver control register (ready bit)
+	andi	$t1, $t1, 1 		# clear all bits except the LSB
+	beq	$t1, $zero, rd_wait	# branch if ready bit not 1
+	lw	$t2, 4($t0)		# load data from receiver data register (actual input)
+
+
+	li	$v0, 10			# exit sys call code
 	syscall
 		
