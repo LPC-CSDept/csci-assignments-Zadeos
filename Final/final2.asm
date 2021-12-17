afe8e8a505e755333a0ca6a4f7f1abdc8aef2b8d
Zadeos <masonjlewis@outlook.com>
2021-12-16 21:13:31 -0800

    getting to work on #2

--- Final/final2.asm
+++ Final/final2.asm
@@ -7,7 +7,11 @@
 # until 'q' is typed.
 
 	.data
-pr1:	.asciiz "enter any characters, quit with 'q' "
+pr1:	.asciiz "enter any characters, quit with 'q'\n"
+
+	.kdata
+thing1:	.word 10	# used for restoring registers
+thing2:	.word 11	# used for restoring registers
 
 	.text
 	.globl main
@@ -15,6 +19,13 @@ main:
 	li	$v0, 4		# print_string sys call code
 	la	$a0, pr1	# address of prompt
 	syscall
+	mfc0	$a0, $t4	# read from the status register
+	li	$a0, 0xFF11	# enables all interrupts
+	mtc0	$a0, $t4	# writes back to the status register
+
+	lui	$t0, 0xFFFF	# Receiver Control memory location
+	li	$a0, 2		# enable keyboard interrupt
+	sw	$a0, 0($t0)	# write back to the receiver data register
 
 	li	$v0, 10		# exit sys call code
 	syscall
