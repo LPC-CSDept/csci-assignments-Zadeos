cd55677bce49ea7524a0ce832ac63bf89c1a2be7
Zadeos <masonjlewis@outlook.com>
2021-12-16 18:24:07 -0800

    just had some food, infinite loop + error

--- Final/final1.asm
+++ Final/final1.asm
@@ -16,27 +16,34 @@ pr1:	.asciiz "please enter 3 numbers (e.g. 123): "
 	.text
 	.globl main
 main:
+	
+
 	li	$v0, 4			# print_string sys call code
 	la	$a0, pr1		# address of prompt
 	syscall
 
 	lui	$t0,  0xFFFF		# Receiver Control memory location
 rd_wait:
+
 	# polling section
 	lw	$t1, 0($t0)		# load from receiver control register (ready bit)
 	andi	$t1, $t1, 1 		# clear all bits except the LSB
 	beq	$t1, $zero, rd_wait	# branch if ready bit not 1
+	nop
 	# loading input and handling
 	# say they enter 123
 	lw	$t2, 4($t0)		# load data from receiver data register (actual input)
-					# $t2 = '1'
+					# $t2 = '1' -> '2' -> '3'
+	nop
 	addiu	$t2, $t2, -47		# subtract 47 to convert from ASCII characters to int 
-					# $t2 = 1
+					# $t2 = 1 -> 2 -> 3
 	li	$t3, 100		# multiplier
-	mul	$t2, $t2, $t3		# $t2 = 100
+	mul	$t2, $t2, $t3		# $t2 = 100 -> 20 -> 3
 	div	$t3, $t3, 10		# adjusting place value by lowering the multiplier
-	addu	$a0, $t2, $t2		# add the output to the argument register to print later
+	addu	$a0, $a0, $t2		# add the output to the argument register to print later
 	bnez	$t3, rd_wait		# if the multplier is 0, then the number has been constructed
+	nop
+
 	li	$v0, 1			# print_int sys call code
 	syscall
 
