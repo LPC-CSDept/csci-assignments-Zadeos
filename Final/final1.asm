c7508cf5870a329704224a4522d605b42b964195
Zadeos <masonjlewis@outlook.com>
2021-12-16 16:31:03 -0800

    some changes

--- Final/final1.asm
+++ Final/final1.asm
@@ -5,6 +5,10 @@
 # Question #5 of Final Exam
 # Program to take digits through MM I/O and display them
 #
+# MIPS Simulation Settings
+# Bare Machine: OFF, Psuedo Instructions: ON, Mapped IO: ON,
+# Delayed Branches: ON, Delayed Loads: ON
+#
 
 	.data
 pr1:	.asciiz "please enter 3 numbers (e.g. 123): "
@@ -18,11 +22,23 @@ main:
 
 	lui	$t0,  0xFFFF		# Receiver Control memory location
 rd_wait:
+	# polling section
 	lw	$t1, 0($t0)		# load from receiver control register (ready bit)
 	andi	$t1, $t1, 1 		# clear all bits except the LSB
 	beq	$t1, $zero, rd_wait	# branch if ready bit not 1
+	# loading input and handling
+	# say they enter 123
 	lw	$t2, 4($t0)		# load data from receiver data register (actual input)
-
+					# $t2 = '1'
+	addiu	$t2, $t2, -47		# subtract 47 to convert from ASCII characters to int 
+					# $t2 = 1
+	li	$t3, 100		# multiplier
+	mul	$t2, $t2, $t3		# $t2 = 100
+	div	$t3, $t3, 10		# adjusting place value by lowering the multiplier
+	addu	$a0, $t2, $t2		# add the output to the argument register to print later
+	bnez	$t3, rd_wait		# if the multplier is 0, then the number has been constructed
+	li	$v0, 1			# print_int sys call code
+	syscall
 
 	li	$v0, 10			# exit sys call code
 	syscall
